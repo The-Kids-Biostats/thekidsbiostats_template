fbfd5929c6d6e22e13e13a27acd43a0b37ed4545
bethymc <147642319+bethymc@users.noreply.github.com>
2024-11-13 11:34:18 +0800

    Create ProjectSetupScript.R
    Set up a new project

---
+++ ProjectSetupScript.R
@@ -0,0 +1,9 @@
+library(thekidsbiostats)
+
+# setwd() not needed if .Rproj created first eg as a New Project from within the Biostats template
+#setwd("Desktop/test_stat/")
+
+create_project("basic")
+
+create_template("report") 
+create_template("word")
